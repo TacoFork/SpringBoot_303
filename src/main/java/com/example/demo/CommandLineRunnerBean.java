fa240fb78413ea65277b8b205d7f9788dc5ee9ce
David Kim <68195597+TacoFork@users.noreply.github.com>
2021-08-09 14:37:11 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/CommandLineRunnerBean.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommandLineRunnerBean implements CommandLineRunner {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    public void run(String...args){
+
+    }
+
+    //big picture = defining own schema (user and role classes) and with this new schema we are creating our own specified users/roles
+}
---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+import java.security.Principal;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    @Autowired
+    CloudinaryConfig cloudc;
+
+
+}
